53f2805eed93efb36822a10e19c0987ce4d96492
Mathias Lauridsen <math227v@gmail.com>
2025-06-24 21:30:04 +0200

    Add: Flowbite

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -1,25 +1,30 @@
 <script lang="ts">
+	import { Button, Input, Drawer, Label, P, Heading, Popover } from 'flowbite-svelte';
 	import { currentuser, pb } from './pocketbase';
 
-	let username: string | undefined = $state();
-	let password: string | undefined = $state();
+	let hideLoginDrawer = $state(true);
+	let hideSignupDrawer = $state(true);
+
+	let name: string | undefined = $state();
 	let email: string | undefined = $state();
+	let password: string | undefined = $state();
 
 	async function login(event: Event) {
 		event.preventDefault();
 		if (email && password) {
 			await pb.collection('users').authWithPassword(email, password);
+			hideLoginDrawer = true;
+			hideSignupDrawer = true;
 		}
 	}
 
 	async function signUp(event: Event) {
 		try {
 			const data = {
-				username,
-				password,
+				name,
 				email,
-				passwordConfirm: password,
-				name: 'hi mom!'
+				password,
+				passwordConfirm: password
 			};
 
 			const createdUser = await pb.collection('users').create(data);
@@ -31,19 +36,78 @@
 
 	function signOut() {
 		pb.authStore.clear();
+		name = undefined;
+		email = undefined;
+		password = undefined;
 	}
 </script>
 
-{#if $currentuser}
-	<p>Signed in as {$currentuser.name}</p>
-	<button onclick={signOut}>Logout</button>
-{:else}
+<!-- {#if !$currentuser}
 	<form>
-		<input type="text" placeholder="username" bind:value={username} />
-		<input type="email" placeholder="email" bind:value={email} />
-		<input type="password" placeholder="password" bind:value={password} />
-
-		<button onclick={signUp}>Sign Up</button>
-		<button onclick={login}>Login</button>
+		<div class="flex flex-row gap-4">
+			<Input type="text" id="email" placeholder="Email" bind:value={email} required />
+			<Input type="password" id="password" placeholder="Password" bind:value={password} required />
+			<Button onclick={login}>Login</Button>
+		</div>
 	</form>
+{:else}
+	<div class="flex flex-row items-center gap-4">
+		{$currentuser.name}
+		<Button onclick={signOut}>Logout</Button>
+	</div>
+{/if} -->
+
+<Drawer bind:hidden={hideLoginDrawer}>
+	<Heading tag="h3" class="mb-4">Login</Heading>
+	<div class="flex flex-col gap-4">
+		<div>
+			<Label for="email" class="mb-2">Email</Label>
+			<Input type="text" id="email" placeholder="Email" bind:value={email} required />
+		</div>
+		<div>
+			<Label for="password" class="mb-2">Password</Label>
+			<Input type="password" id="password" placeholder="Password" bind:value={password} required />
+		</div>
+		<Button onclick={login}>Login</Button>
+	</div>
+</Drawer>
+
+<Drawer bind:hidden={hideSignupDrawer}>
+	<Heading tag="h3" class="mb-4">Signup</Heading>
+	<div class="flex flex-col gap-4">
+		<div>
+			<Label for="name" class="mb-2">Name</Label>
+			<Input
+				type="text"
+				id="name"
+				placeholder="What should we call you by?"
+				bind:value={name}
+				required
+			/>
+		</div>
+		<div>
+			<Label for="email" class="mb-2">Email</Label>
+			<Input type="text" id="email" placeholder="Email" bind:value={email} required />
+		</div>
+		<div>
+			<Label for="password" class="mb-2">Password</Label>
+			<Input type="password" id="password" placeholder="Password" bind:value={password} required />
+		</div>
+		<Button onclick={signUp}>Login</Button>
+	</div>
+</Drawer>
+
+{#if !$currentuser}
+	<div class="flex flex-row items-center gap-4">
+		<Button onclick={() => (hideLoginDrawer = false)} color="alternative">Login</Button>
+		<Button onclick={() => (hideSignupDrawer = false)}>Signup</Button>
+	</div>
+{:else}
+	<div class="flex flex-row items-center gap-4">
+		<P>Welcome, {$currentuser.name}</P>
+		<Button>Logout</Button>
+		<Popover trigger="click" triggerDelay={0}>
+			<Button onclick={signOut}>For real?</Button>
+		</Popover>
+	</div>
 {/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,32 @@
 <script lang="ts">
+	import Login from '$lib/Login.svelte';
 	import '../app.css';
+
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+<Navbar>
+	<NavBrand href="/">
+		<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
+			>Spock chat</span
+		>
+	</NavBrand>
+	<NavHamburger />
+	<NavUl>
+		<NavLi href="/">Home</NavLi>
+		<NavLi href="/about">About</NavLi>
+		<NavLi href="/docs/components/navbar">Navbar</NavLi>
+		<NavLi href="/pricing">Pricing</NavLi>
+		<NavLi href="/contact">Contact</NavLi>
+	</NavUl>
+
+	<div class="flex md:order-2">
+		<Login />
+		<NavHamburger />
+	</div>
+</Navbar>
+<div class="container mx-auto">
+	{@render children()}
+</div>
