cd245a4be42044e54801532bf26dee87adbd060e
Mathias Lauridsen <math227v@gmail.com>
2025-06-24 23:35:07 +0200

    Add: Avatars

--- src/lib/Messages.svelte
+++ src/lib/Messages.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { onMount, onDestroy } from 'svelte';
 	import { currentuser, pb } from './pocketbase';
-	import { Button, Heading, Input, P } from 'flowbite-svelte';
+	import { Avatar, Button, Heading, Input, P } from 'flowbite-svelte';
 	import type { ListResult, RecordModel } from 'pocketbase';
 
 	let newMessage: string = '';
@@ -47,9 +47,12 @@
 <Heading tag="h1">Messages</Heading>
 <div class="my-4 flex max-h-96 flex-col gap-4 overflow-scroll rounded border-2 border-gray-300 p-2">
 	{#each messages as message (message?.id)}
-		<div class="rounded-xl border-2 border-gray-300 p-2">
-			<P>{message?.text}</P>
-			<small>Sent by @{message?.expand?.user?.name}</small>
+		<div class="flex flex-row items-center gap-2 rounded-xl border-2 border-gray-300 p-4">
+			<Avatar src="https://api.dicebear.com/9.x/thumbs/svg?seed={message?.expand?.user?.name}" />
+			<div class="flex flex-1 flex-col">
+				<P>{message?.text}</P>
+				<small>Sent by @{message?.expand?.user?.name}</small>
+			</div>
 		</div>
 	{/each}
 </div>
