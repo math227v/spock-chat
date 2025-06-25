2848eb013573b1f958704d8ae651889dc2c4300c
Mathias Lauridsen <math227v@gmail.com>
2025-06-25 22:25:48 +0200

    Add: Realtime Position Updates

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -3,42 +3,59 @@
 	import { MapPinAltSolid } from 'flowbite-svelte-icons';
 	import { MapLibre, Marker, type MarkerClickInfo } from 'svelte-maplibre';
 	import { currentuser, pb } from './pocketbase';
-	import type { RecordModel } from 'pocketbase';
+	import type { ListResult, RecordModel } from 'pocketbase';
+	import { Avatar } from 'flowbite-svelte';
 
 	let userPos: { coords: [number, number]; accuracy: number } | undefined = $state(undefined);
 	let locationError: string | undefined = $state(undefined);
 
-	let positions: Array<RecordModel | undefined> = $state([]);
+	let positions: RecordModel[] | undefined = $state();
 	let unsubscribe: () => void;
 
 	onMount(() => {
 		pb.collection('positions')
-			.getFullList()
+			.getList(1, 50, { expand: 'user' })
 			.then(async (data) => {
-				positions = data;
-
+				if (data?.items) {
+					positions = data.items;
+					console.dir(positions);
+				} else {
+					positions = [];
+				}
 				unsubscribe = await pb
 					.collection('positions')
 					.subscribe('*', async ({ action, record }) => {
 						if (action === 'create') {
 							// Tilføj ny position til arrayet
-							positions.push(record);
+							const user = await pb.collection('users').getOne(record.user);
+							record.expand = { user };
+							if (positions) {
+								positions.push(record);
+							} else {
+								positions = [record];
+							}
 						} else if (action === 'delete') {
 							// Find index på den slettede position
-							const index = positions.findIndex((p) => p?.id === record.id);
-							if (index !== -1) {
-								// Fjern den slettede position fra arrayet
-								positions.splice(index, 1);
+							if (positions) {
+								const index = positions.findIndex((p) => p?.id === record.id);
+								if (index !== -1) {
+									// Fjern den slettede position fra arrayet
+									positions.splice(index, 1);
+								}
 							}
 						} else if (action === 'update') {
-							// Find index på den opdaterede position i positions-arrayet
-							const index = positions.findIndex((p) => p?.id === record.id);
+							if (positions) {
+								// Find index på den opdaterede position i positions-arrayet
+								const index = positions.findIndex((p) => p?.id === record.id);
 
-							if (index !== -1) {
-								// Opdater den eksisterende position med det nye record
-								positions[index] = record;
+								if (index !== -1) {
+									// Opdater den eksisterende position med det nye record
+									positions[index].position = record.position;
+								} else {
+									positions.push(record);
+								}
 							} else {
-								positions.push(record);
+								positions = [record];
 							}
 						}
 					});
@@ -68,18 +85,32 @@
 						locationError = 'Lokationen er ikke præcis nok.';
 					}
 
-					pb.collection('positions')
-						.getFirstListItem(`user="${$currentuser?.id}"`)
-						.catch(() => null)
-						.then((existing) => {
-							const data = {};
+					if ($currentuser) {
+						pb.collection('positions')
+							.getFirstListItem(`user="${$currentuser.id}"`)
+							.catch(() => null)
+							.then((existing) => {
+								const data = {
+									position: {
+										lon: pos.coords.longitude,
+										lat: pos.coords.latitude
+									},
+									user: $currentuser.id
+								};
 
-							if (existing) {
-								pb.collection('positions').update(existing.id, data);
-							} else {
-								pb.collection('positions').create(data);
-							}
-						});
+								if (existing) {
+									console.log('Updating pos');
+									pb.collection('positions')
+										.update(existing.id, data)
+										.catch((err) => console.error(err));
+								} else {
+									console.log('Creating pos');
+									pb.collection('positions')
+										.create(data)
+										.catch((err) => console.error(err));
+								}
+							});
+					}
 
 					userPos = data;
 				},
@@ -108,18 +139,25 @@
 		style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
 	>
 		<!-- <NavigationControl /> -->
-		{#if userPos?.coords}
+		<!-- {#if userPos?.coords}
 			<Marker bind:lngLat={userPos.coords}>
 				<MapPinAltSolid class="-translate-y-1/2" size="xl" color="#ff0000" />
 			</Marker>
-		{/if}
+		{/if} -->
 
-		{#each positions as entry (entry?.id)}
-			{#if entry?.position}
-				<Marker bind:lngLat={entry.position}>
-					<MapPinAltSolid class="-translate-y-1/2 cursor-pointer" size="xl" color="#ff9900" />
-				</Marker>
-			{/if}
-		{/each}
+		{#if positions}
+			{#each positions as entry (entry?.id)}
+				{#if entry?.position}
+					<Marker bind:lngLat={entry.position}>
+						<Avatar
+							src="https://api.dicebear.com/9.x/thumbs/svg?seed={entry?.expand?.user?.id}"
+							border={$currentuser?.id == entry?.expand?.user?.id}
+							class="ring-green-400"
+						/>
+						<!-- <MapPinAltSolid class="-translate-y-1/2 cursor-pointer" size="xl" color="#ff9900" /> -->
+					</Marker>
+				{/if}
+			{/each}
+		{/if}
 	</MapLibre>
 </div>
